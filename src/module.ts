5e29942053fab670cc0761f7718b6349b1114ce5
Unknown <froastj@gmail.com>
2019-06-10 21:57:14 -0700

    Add module dependencies
    See f8b16245c6bb8aabab0b6b1b3b7a5e4bbd88cc05

--- src/module.ts
+++ src/module.ts
@@ -3,13 +3,23 @@ import {Bot} from "./bot";
 export class Module {
     public readonly name: string;
     public readonly bot: Bot;
+    public readonly dependencies: string[];
 
-    constructor(bot: Bot, name: string) {
+    constructor(bot: Bot, name: string, dependencies?: string[]) {
         this.bot = bot;
         this.name = name;
+        if (dependencies == null) {
+            this.dependencies = [];
+        } else {
+            this.dependencies = dependencies;
+        }
     }
 
     public async initialize(): Promise<void> {
+        return Promise.resolve();
+    }
+
+    public async loadDependencies(): Promise<void> {
         return Promise.resolve();
     }
 }

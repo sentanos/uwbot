fc653d7ebd4b7c9fda3f836b3216a47e90f10281
Unknown <froastj@gmail.com>
2019-09-06 10:55:01 -0700

    Add comments and remove unused functions

--- src/util.ts
+++ src/util.ts
@@ -6,6 +6,7 @@ import {Availability, CommandCategory, CommandsModule, Permission} from "./modul
 import {Bot} from "./bot";
 import {ChannelAddCommand, ChannelGetCommand, ChannelRemoveCommand} from "./commands/channels.tmpl";
 
+// Returns items in the array separated by newlines or the string "_None_" if the array is empty
 export const listOrNone = (arr: string[]): string => {
     return arr.length === 0 ? "_None_" : arr.join("\n");
 };
@@ -15,6 +16,7 @@ export const random = (max: number): number => {
     return Math.floor(Math.random() * max);
 };
 
+// Returns a random valid discord color in decimal form
 export const randomColor = (): number => {
     return random(16777215);
 };
@@ -23,6 +25,7 @@ export const generateUID = (): string => {
     return uuid();
 };
 
+// Returns the given array sorted in alphabetical order (a-z)
 export const alphabetical = (arr: string[]): string[] => {
     return arr.sort((a: string, b: string): number => {
         if (a < b) {
@@ -35,6 +38,10 @@ export const alphabetical = (arr: string[]): string[] => {
     });
 };
 
+// Given an interval in seconds, returns the number of days, hours, minutes, or seconds it
+// is equal to. Note that it will only return one of these, not a combination, and will only return
+// the interval of the largest interval it is _exactly equal to_. For example, 86400 seconds
+// would return 1 day, but 86460 would return 25 hours.
 export const formatInterval = (seconds: number): string => {
     let value: number;
     let unit: string;
@@ -58,6 +65,7 @@ export const formatInterval = (seconds: number): string => {
     }
 };
 
+// Returns a cryptographically safe random string that uses hex characters
 export const randomString = (length: number): Promise<string> => {
     return new Promise((resolve, reject) => {
         randomBytes(length, (err: Error, buf: Buffer) => {
@@ -75,14 +83,7 @@ export const timeDiff = (t1: Date, t2: Date): number => {
     return t1.getTime() - t2.getTime();
 };
 
-export const toSQLiteDate = (date: Date): string => {
-    return date.toISOString().replace("T", " ").replace("Z","");
-};
-
-export const fromSQLiteDate = (date: string): Date => {
-    return new Date(date + " UTC");
-};
-
+// Returns the nth index of substring within string
 export const getNthIndex = (str: string, substr: string, n: number): number => {
     let i = -1;
 
@@ -164,14 +165,19 @@ type ChannelListStatic = typeof Model & {
     new (values?: object, options?: BuildOptions): ChannelList;
 }
 
+// A list of channels that automatically saves to the database and can add commands to manage and
+// view the list
 export class PersistentChannelList {
     private readonly DB: Sequelize;
     private readonly bot: Bot;
     private list: ChannelListStatic;
     private cache: Set<Snowflake>;
 
+    // Note: Both initialize and the constructor must be called to fully load the class
     constructor(bot: Bot, name: string) {
         this.DB = bot.DB;
+        // The table used in the database is the name of the given channel list. Care should be
+        // taken to not give it the same name as an existing table.
         this.list = <ChannelListStatic>this.DB.define(name, {
             channelID: {
                 primaryKey: true,
@@ -182,6 +188,7 @@ export class PersistentChannelList {
         this.cache = new Set<Snowflake>();
     }
 
+    // Asynchronously prepares the class
     public async initialize(): Promise<void> {
         await this.list.sync();
         (await this.list.findAll()).forEach((channel: ChannelList) => {
@@ -189,6 +196,13 @@ export class PersistentChannelList {
         });
     }
 
+    // Given a configuration, creates the following commands:
+    //     get: Shows the channel list with the names of the channels if possible and the IDs of
+    //          the channels if they do not exist
+    //     set: Given a channel ID, adds it to the list
+    //     remove: Given a channel ID, removes it from the list
+    // Each command is created with the name, usage description, permission, and availability
+    // specified in the config.
     public addCommands(config: PersistentChannelListConfig): void {
         const commandsMod = this.bot.getModule("commands") as CommandsModule;
         commandsMod.addCommand(new ChannelGetCommand(this.bot, config.parentModule, this,
@@ -199,6 +213,7 @@ export class PersistentChannelList {
             config.remove, config.listName.toLowerCase()));
     }
 
+    // Returns an array of channel IDs that are in the list
     public async getChannels(): Promise<Snowflake[]> {
         let channels: Snowflake[] = [];
         const rows = await this.list.findAll();
@@ -208,10 +223,13 @@ export class PersistentChannelList {
         return channels;
     }
 
+    // Returns true if the given channel ID is in the list, false otherwise
     public async has(channel: Snowflake): Promise<boolean> {
         return this.cache.has(channel);
     }
 
+    // Adds the given channel ID to the list
+    // Throws an error if the channel has already been added to the list
     public async add(channel: Snowflake): Promise<void> {
         if (await this.has(channel)) {
             throw new Error("SAFE: Channel has already been added");
@@ -222,6 +240,8 @@ export class PersistentChannelList {
         this.cache.add(channel);
     }
 
+    // Removes a channel ID from the list
+    // Throws an error if the channel is not in the list
     public async remove(channel: Snowflake): Promise<void> {
         if (!(await this.has(channel))) {
             throw new Error("SAFE: Channel not in list");

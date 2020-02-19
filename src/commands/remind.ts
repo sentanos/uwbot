4f574124c7123b40bec21d1a2a9fd83657110533
Unknown <froastj@gmail.com>
2020-02-19 03:24:21 -0800

    Allow interval and datetime for remindme

--- src/commands/remind.ts
+++ src/commands/remind.ts
@@ -1,17 +1,15 @@
 import {
     Availability,
     Command,
-    CommandConfig, CommandsModule,
+    CommandConfig,
     PartialCommandConfig,
     Permission
 } from "../modules/commands";
 import {Bot} from "../bot";
 import {Message} from "discord.js";
 import {RemindModule} from "../modules/remind";
-import {
-    IntervalResponse,
-    smartFindInterval
-} from "../util";
+import {TimeModule} from "../modules/time";
+import {dateAfter, smartFindDuration} from "../util";
 
 class RequiresRemind extends Command {
     protected remind: RemindModule;
@@ -33,7 +31,10 @@ export class RemindMe extends RequiresRemind {
         super(bot, {
             names: ["remindme", "remind"],
             usages: {
-                "Gives you a reminder after the given interval": ["interval", "reminder"]
+                "Gives you a reminder after the given interval": ["reminder", "interval"],
+                ["Gives you a reminder on a certain date and (optionally) time. Supported" +
+                " formats for date: YYYY-MM-DD, MM/DD/YYY. Supported formats for time: 23:59," +
+                " 11:59pm"]: ["reminder", "date", "time"]
             },
             permission: Permission.VerifiedGuildMember,
             availability: Availability.All
@@ -41,7 +42,18 @@ export class RemindMe extends RequiresRemind {
     }
 
     async exec(message: Message, input: string, _: string): Promise<Message> {
-        const resp: IntervalResponse = smartFindInterval(this.bot, message.content, false);
-        return await this.remind.createReminder(message, resp.raw, resp.interval);
+        const timeModule = this.bot.getModule("time") as TimeModule;
+        let raw;
+        let date;
+        try {
+            const resp = timeModule.smartFindDate(message.content);
+            raw = resp.raw;
+            date = resp.date;
+        } catch (e) {
+            const resp = smartFindDuration(this.bot, message.content, false);
+            raw = resp.raw;
+            date = dateAfter(resp.duration);
+        }
+        return await this.remind.createReminder(message, raw, date);
     }
 }

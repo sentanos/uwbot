cf885de8a461e6b868304c2a2e15d039227ac32c
Joshua Lanese <froastj@gmail.com>
2020-06-21 17:34:06 -0700

    Add after-init functions for loading models
    Some models need to run additional steps after initialization

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,22 @@
 import {Sequelize} from "sequelize";
 import {Bot} from "../bot";
 
-export default (sequelize: Sequelize): Promise<number> => {
-    return Bot.forEachClassInFile("./database/models",
-        async (name: string, init: any): Promise<boolean> => {
+export default async (sequelize: Sequelize): Promise<number> => {
+    let after = [];
+    const count = await Bot.forEachClassInFile("./database/models",
+        async (name: string, func: any): Promise<boolean> => {
             if (name === "init") {
-                init(sequelize);
+                func(sequelize);
                 return true;
+            } else if (name === "after") {
+                after.push(func);
             }
             return false;
         });
+
+    for (let i = 0; i < after.length; i++) {
+        after[i]();
+    }
+
+    return count;
 }
